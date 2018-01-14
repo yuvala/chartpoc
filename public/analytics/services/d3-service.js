2f306141f2f42959878c3db2de06e8a63e121d69
yuval almaliah <yuv.almaliah@hmail.com>
2018-01-14 08:40:57 +0200

    1

--- public/analytics/services/d3-service.js
+++ public/analytics/services/d3-service.js
@@ -12,8 +12,8 @@ angular.module('d3', [])
       var scriptTag = $document[0].createElement('script');
       scriptTag.type = 'text/javascript';
       scriptTag.async = true;
-      // scriptTag.src = 'http://d3js.org/d3.v3.min.js';
-      scriptTag.src = '../node_modules/d3/build/d3.js';
+      scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+      //scriptTag.src = '../node_modules/d3/d3.js';
       scriptTag.onreadystatechange = function () {
         if (this.readyState == 'complete') onScriptLoad();
       }
--- public/app.js
+++ public/app.js
@@ -4,4 +4,4 @@ angular.module('brainlab.analytics', []);
 angular.module('d3', []);
  
 
- 
+ //http://www.ng-newsletter.com/posts/d3-on-angular.html 
